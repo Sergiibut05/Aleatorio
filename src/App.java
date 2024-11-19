a388e773baf39e294a524f911f3c3395f4fa39c8
Sergiibut05 <sergiibutrii555@gmail.com>
2024-11-19 20:12:35 +0100

    commit 3

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ import Ejercicio3.Ejercicio3;
 import Ejercicio4.Ejercicio4;
 import Ejercicio5.Ejercicio5;
 import Ejercicio6.Ejercicio6;
+import Ejercicio6.Ejercicio6_2;
 public class App {
     public static void main(String[] args) throws Exception {
         //Ejercicio1.ejercicio1();
@@ -11,6 +12,7 @@ public class App {
         //Ejercicio3.ejercicio3();
         //Ejercicio4.ejercicio4();
         //Ejercicio5.ejercicio5();
-        Ejercicio6.ejercicio6();
+        //Ejercicio6.ejercicio6();
+        Ejercicio6_2.ejemplo6();
     }
 }
--- src/Ejercicio6/Ejercicio6_2.java
+++ src/Ejercicio6/Ejercicio6_2.java
@@ -1,26 +1,52 @@
 package Ejercicio6;
 
 public class Ejercicio6_2 {
-    public static void ejemplo6(){
-        System.out.println("Introduce un número: ");
-        int numaleatorio=Integer.parseInt(System.console().readLine());
-        int num=0;
-        for(int i=1;i<=5;i++){
-            System.out.print("Introduce un número: ");
-            num=Integer.parseInt(System.console().readLine());
-            if(num>numaleatorio){
-                System.out.println("El número que estoy pensando es menor que "+num);
-            }else{
-                if(num<numaleatorio){
-                    System.out.println("El número que estoy pensando es mayor que "+num);
-                }else{
-                    System.out.println("¡Felecidades! Has Acertado");
-                    i=8;
+    public static void ejemplo6() {
+        System.out.println("Introduce un número entre 1 y 100: ");
+        int num = Integer.parseInt(System.console().readLine()); // Número a adivinar
+        int intentos = 5; // Máximo de intentos permitidos
+        int rangoMaximo = (int) Math.ceil(Math.log(100) / Math.log(2)); // Logaritmo base 2
+        int numaleatorio; // Número que el programa intenta adivinar
+        int nummin = 1; // Límite inferior del rango
+        int nummax = 100; // Límite superior del rango
+
+        if (rangoMaximo <= intentos) {
+            // Puede acertar seguro: aplica búsqueda binaria
+            for (int i = 1; i <= intentos; i++) {
+                numaleatorio = nummin + (nummax - nummin) / 2;
+                System.out.println("Intento " + i + ": ¿Es " + numaleatorio + "?");
+
+                if (num == numaleatorio) {
+                    System.out.println("¡Felicidades! Has acertado.");
+                    return;
+                } else if (num > numaleatorio) {
+                    System.out.println("El número que estoy pensando es mayor que " + numaleatorio);
+                    nummin = numaleatorio + 1; // Actualiza el límite inferior
+                } else {
+                    System.out.println("El número que estoy pensando es menor que " + numaleatorio);
+                    nummax = numaleatorio - 1; // Actualiza el límite superior
+                }
+            }
+        } else {
+            // No puede acertar seguro: elige aleatoriamente
+            for (int i = 1; i <= intentos; i++) {
+                numaleatorio = nummin + (int) (Math.random() * (nummax - nummin + 1));
+                System.out.println("Intento " + i + ": ¿Es " + numaleatorio + "?");
+
+                if (num == numaleatorio) {
+                    System.out.println("¡Felicidades! Has acertado.");
+                    return;
+                } else if (num > numaleatorio) {
+                    System.out.println("El número que estoy pensando es mayor que " + numaleatorio);
+                    nummin = numaleatorio + 1;
+                } else {
+                    System.out.println("El número que estoy pensando es menor que " + numaleatorio);
+                    nummax = numaleatorio - 1;
                 }
             }
         }
-        if(num!=numaleatorio){
-            System.out.println("Que pena, no has acertado");
-        }
+
+        // Si no lo adivina en el número de intentos permitido
+        System.out.println("Qué pena, no he acertado. El número era " + num);
     }
 }
